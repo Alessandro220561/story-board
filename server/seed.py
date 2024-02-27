5e3490148fa876643c2c2f4513840ed5fa2ee6df
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-02-27 18:53:13 -0500

    migration versions initialization for users and books

--- server/seed.py
+++ server/seed.py
@@ -15,3 +15,23 @@ if __name__ == '__main__':
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+from config import app, db
+from models import User, Book
+
+if __name__ == "__main__":
+
+    with app.app_context():
+
+        User.query.delete()
+        Book.query.delete()
+
+        u1 = User(username="Alex Shaw", email="alex@gmail.com")
+        u2 = User(username="Jonathan Sherry", email="johnsher@yahoo.com")
+        db.session.add_all([u1, u2])
+        db.session.commit()
+
+        b1 = Book(title="IT", author="Stephen King", genre="Horror", pages=645)
+        b2 = Book(title="Malice", author="John Gwynne", genre='Fantasy', pages=50)
+        db.session.add_all([b1, b2])
+        db.session.commit()
