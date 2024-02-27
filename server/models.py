588da5544d1e23a60e517b2f9aac629bbb73da94
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-02-27 18:39:28 -0500

    create new books table/model

--- server/models.py
+++ server/models.py
@@ -1,1 +1,20 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
 
+from config import db
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String)
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    genre = db.Column(db.String)
+    pages = db.Column(db.Integer)
