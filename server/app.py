0f1315b250c34fcc4a380f0c74d7b298bed2d416
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-02-27 18:35:59 -0500

    db initialization

--- server/app.py
+++ server/app.py
@@ -20,3 +20,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
---
+++ server/migrations/versions/9b2a0d5d1e9e_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 9b2a0d5d1e9e
+Revises: 
+Create Date: 2024-02-27 18:33:12.097131
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9b2a0d5d1e9e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
