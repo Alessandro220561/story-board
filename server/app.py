5ef0fce5249c9448d80f475acc4f377cb4a96b50
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-02-27 19:01:46 -0500

    succesful migration of users and books table

--- server/app.py
+++ server/app.py
@@ -3,19 +3,24 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, session, jsonify
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import User, Book
 
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+def Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(jsonify(users), 200)
 
 
 if __name__ == '__main__':
