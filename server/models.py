cb982d02c9da8cb554eccd9ab52586a9139cd322
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-03-04 20:06:36 -0500

    add password column to users table

--- server/models.py
+++ server/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
@@ -9,7 +10,22 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     email = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
     serialize_rules = ('-reading_logs.user', )
+
     reading_logs = db.relationship("ReadingLog", back_populates="user")
 
 class Book(db.Model, SerializerMixin):
@@ -20,7 +36,9 @@ class Book(db.Model, SerializerMixin):
     author = db.Column(db.String)
     genre = db.Column(db.String)
     pages = db.Column(db.Integer)
+
     serialize_rules = ('-reading_logs.book', )
+
     reading_logs = db.relationship("ReadingLog", back_populates="book")
 
 class ReadingLog(db.Model, SerializerMixin):
@@ -35,7 +53,6 @@ class ReadingLog(db.Model, SerializerMixin):
     serialize_rules = ('-user.reading_logs', '-book.reading_logs')
 
     user = db.relationship("User", back_populates="reading_logs")
-
     book = db.relationship("Book", back_populates="reading_logs")
 
 # class UserLog(db.Model, SerializerMixin):
